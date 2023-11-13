becc837aef290ac375d56ca3952f0843f503e18a
Yasamin Nouri Jelyani <yasamin.nourijelyani@mail.utoronto.ca>
2023-11-13 13:36:48 -0500

    fixed data preprocess cleanup - and will be using subset of data

--- data_preprocess/get_reports.sh
+++ data_preprocess/get_reports.sh
@@ -16,7 +16,10 @@ cd "$output_dir"
 # Run wget 
 # wget -r -N -c -np --user "$username" --ask-password -A .jpg -I /files/mimic-cxr/2.0.0/files/p10/p10000032 "$base_url"
 #wget -r -N -c -np --user "$username" --ask-password https://physionet.org/files/mimic-cxr-jpg/2.0.0/
-wget -r -N -c -np -A .txt --user "$username" --ask-password https://physionet.org/files/mimic-cxr/2.0.0/
+# All data get
+#wget -r -N -c -np -A .txt --user "$username" --ask-password https://physionet.org/files/mimic-cxr/2.0.0/
+#Subset data get
+wget -r -N -c -np -A .txt --user "$username" --ask-password https://physionet.org/files/mimic-cxr/2.0.0/files/p10/p10000032/
 # Return to the original directory
 cd -
 
