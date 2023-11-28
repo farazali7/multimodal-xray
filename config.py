ece10ae61cc89fa984194ce099bc674b3e7425c2
Faraz Ali <77faraz77@gmail.com>
2023-11-27 20:56:25 -0500

    paramETERS

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ cfg = {
             'enable_checkpointing':True,
             'accelerator':'gpu',
             'devices':int(torch.cuda.device_count()),
-            'strategy':'ddp_find_unused_params_true'
+            'strategy':'ddp_find_unused_parameters_true'
         },
         'LR': 5e-4,
         'BATCH_SIZE': 16
